b145fac0c63ee3d3174191875f395460bcb20f7d
Yash <67926590+Yash094@users.noreply.github.com>
2025-01-25 13:35:40 +0530

    Add SKALE Nebula Hub Testnet

--- src/app/connect-button/page.tsx
+++ src/app/connect-button/page.tsx
@@ -1,17 +1,19 @@
 "use client";
 import type { NextPage } from "next";
-import { ConnectButton } from "thirdweb/react";
+import { ConnectButton, useActiveAccount } from "thirdweb/react";
 import { client } from "../../lib/client";
 import { generatePayload, isLoggedIn, login, logout } from "./actions/auth";
-import { defineChain } from "thirdweb";
+import { defineChain, getContract, deploySmartAccount, Hex } from "thirdweb";
 
 
 const ConnectButtonPage: NextPage = () => {
+  const account = useActiveAccount();
+  
   return (
     <ConnectButton
-      client={client}
+      client={client} 
       accountAbstraction={{
-        chain: defineChain(17000),
+        chain: defineChain(37084624),
         sponsorGas: true,
       }}
       auth={{
@@ -23,7 +25,20 @@ const ConnectButtonPage: NextPage = () => {
           console.log("logging in!");
           await login(params);
         },
-        getLoginPayload: async ({ address }) => generatePayload({ address, chainId: 17000 }),
+        getLoginPayload: async ({ address }) => {
+          await deploySmartAccount({
+            //@ts-ignore
+            smartAccount: account,
+            accountContract: getContract({
+              client,
+              address: account?.address as Hex,
+              chain: defineChain(37084624),
+            }),
+            chain: defineChain(37084624),
+            client
+          })
+          return generatePayload({ address, chainId: 37084624 })
+        },
         doLogout: async () => {
           console.log("logging out!");
           await logout();
